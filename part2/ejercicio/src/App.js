6f94cada706a3227a86af2bb39ec75cfcb175d4e
NotTheJerry <quinterodiego208@gmail.com>
2023-12-20 23:31:53 -0600

    Finalizacion de la parte 1.b "Formularios" y los ejercicios 2.6-2.10"

--- part2/ejercicio/src/App.js
+++ part2/ejercicio/src/App.js
@@ -1,56 +1,61 @@
-import React from "react";
-
-import Course from "./components/Course";
+import React, { useState } from 'react'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import Filter from './components/Filter'
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumero, setNewNumero ] = useState('')
+  const [filtro, setFiltro] = useState('')
+
+  const agregarPersona = (event) => {
+    event.preventDefault()
+    const nuevaPersona = {
+      name: newName,
+      number: newNumero
+    }
+    if(persons.find(person => person.name === newName)){
+      return alert(`${newName} is already added to phonebook`)
+    }
+    setPersons(persons.concat(nuevaPersona))
+    setNewName('')
+    setNewNumero('')
+  }
+
+  const handleNuevoNombre = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNuevoNumero = (event) => {
+    setNewNumero(event.target.value)
+  }
+
+  const handleFiltro = (event) => {
+    setFiltro(event.target.value)
+  }
+
+  const personsFiltrated = persons.filter(person => person.name.toUpperCase() === filtro.toUpperCase())
+  
 
   return (
-    courses.map(course => <Course key={course.id} course={course} /> )
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filtro={filtro} handleFiltro={handleFiltro} personsFiltrated={personsFiltrated}/>
+      <h2>add a new</h2>
+      <PersonForm agregarPersona={agregarPersona} 
+      newName={newName} handleNuevoNombre={handleNuevoNombre} 
+      newNumero={newNumero} handleNuevoNumero={handleNuevoNumero}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={persons} />
+    </div>
   )
 }
 
-export default App;
+export default App
