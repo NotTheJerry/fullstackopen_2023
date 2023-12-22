ae104a2f4bf222efcfecd035721cdea6c5e21b7b
NotTheJerry <quinterodiego208@gmail.com>
2023-12-22 00:56:14 -0600

    Terminando la parte 2.c "Obteniendo datos del servidor", ademas finalice los ejercicios 2.11-2.13, falto ejercicio 2.14 que es conectarse a una API del clima

--- part2/ejercicio/src/App.js
+++ part2/ejercicio/src/App.js
@@ -1,65 +1,85 @@
 import React, { useEffect, useState } from 'react'
-import Persons from './components/Persons'
-import PersonForm from './components/PersonForm'
-import Filter from './components/Filter'
 import axios from 'axios'
 
+const Pais = ({pais}) => {
+    const languages = Object.values(pais.languages)
+    return (
+      <div>
+        <h1>{pais.name.common}</h1>
+        <div>capital: {pais.capital}</div>
+        <div>population: {pais.population}</div>
+        <h3>languages</h3>
+        <ul>{languages.map(language => (
+          <li key={language}>{language}</li>
+        ))}</ul>
+        <img src={pais.flags.png} alt={"Imagen de la bandera de " + pais.name.common} />
+      </div>
+    )
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumero, setNewNumero ] = useState('')
+  const [paises, setPaises] = useState([]) 
   const [filtro, setFiltro] = useState('')
 
   const hook = () => {
     axios
-    .get("http://localhost:3001/persons")
+    .get("https://restcountries.com/v3.1/all")
     .then(response => {
-      setPersons(response.data)
+      setPaises(response.data)
     })
   }
 
   useEffect(hook, [])
-
-  const agregarPersona = (event) => {
-    event.preventDefault()
-    const nuevaPersona = {
-      name: newName,
-      number: newNumero
-    }
-    if(persons.find(person => person.name === newName)){
-      return alert(`${newName} is already added to phonebook`)
-    }
-    setPersons(persons.concat(nuevaPersona))
-    setNewName('')
-    setNewNumero('')
-  }
-
-  const handleNuevoNombre = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNuevoNumero = (event) => {
-    setNewNumero(event.target.value)
-  }
-
+  
   const handleFiltro = (event) => {
     setFiltro(event.target.value)
   }
 
-  const personsFiltrated = persons.filter(person => person.name.toUpperCase() === filtro.toUpperCase())
-  
+  const toggleInfo = (name) => {
+    const pais = paises.find(pais => pais.name.common === name)
+    const cambioPais = { ...pais, importancia: !pais.importancia }
+    setPaises(paises.map(pais => pais.name.common !== name ? pais : cambioPais))
+  }
+
+
+  const mostrarPaises = () => {
+    if(!filtro) {
+      return (<div>You haven't typed anything</div>)
+    }
+
+    const paisesFiltrados = paises.filter(pais => pais.name.common.toUpperCase().includes(filtro.toUpperCase()))
+
+    if(paisesFiltrados.length > 10){
+      return (
+        <div>
+          Too many matches
+        </div>
+      )
+    }
+
+    if(paisesFiltrados.length > 1 && paisesFiltrados.length <=10){
+      return paisesFiltrados.map(pais => {
+          return (
+            <div key={pais.name.common}>
+              {pais.name.common + ' '}
+              <button onClick={() => toggleInfo(pais.name.common) }>{pais.importancia ? 'show less' : 'show'}</button>
+              {pais.importancia ? <Pais pais={pais} /> : ''}
+            </div>
+          )
+        }
+      )
+    }
+
+    if(paisesFiltrados.length === 1){
+     return <Pais pais={paisesFiltrados[0]} />
+    }
+    
+  }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <Filter filtro={filtro} handleFiltro={handleFiltro} personsFiltrated={personsFiltrated}/>
-      <h2>add a new</h2>
-      <PersonForm agregarPersona={agregarPersona} 
-      newName={newName} handleNuevoNombre={handleNuevoNombre} 
-      newNumero={newNumero} handleNuevoNumero={handleNuevoNumero}
-      />
-      <h2>Numbers</h2>
-      <Persons persons={persons} />
+      <div>find countries <input value={filtro} onChange={handleFiltro}/> </div> <br />
+      {mostrarPaises()}
     </div>
   )
 }
